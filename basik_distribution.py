47177fe5b8a298127009761f097505f8a3b0b549
namenaro <nanenaro@gmail.com>
2023-12-14 18:01:55 +0300

    rtsgdhg

--- basik_distribution.py
+++ basik_distribution.py
@@ -15,13 +15,14 @@ class KDistr:
 
     def get_sample(self):
         sample = []
-        for x in range(len(signal) - inertia):
+        max_x = len(self.signal) - self.inertia-1
+        for x in range(max_x):
             a = self._get_one_element(x)
             sample.append(a)
         return sample
 
     def _get_one_element(self, x):
-        ys = self.signal[x:x + self.inertia]
+        ys = self.signal[x:x + self.inertia+1]
         ks = self._get_segments_ks(ys)
         k = mean(ks)
         return k
@@ -35,15 +36,32 @@ class KDistr:
             ks.append(k)
         return ks
 
+class Samples:
+    def __init__(self, signal, inertia):
+        self.all = KDistr(signal[11:-1], inertia).get_sample()
 
+        positive_reg = signal[11:43]
+        self.poss = KDistr(positive_reg, inertia).get_sample()
+
+        negative_reg = signal[44:-1]
+        self.negs = KDistr(negative_reg, inertia).get_sample()
+
+
+    def get_all_pos_neg(self):
+        return self.all, self.poss, self.negs
 
 if __name__ == '__main__':
     signal = get_signal_snippet(lead_name='i', start_coord=340, end_coord=435)
     log = HtmlLogger("K_LOG")
+    log.add_text("Гистограммы реальных К-интерционного (по всему сигналу, в позитивном регионе, в негативном)")
 
     for inertia in range(2, 16, 2):
-        sample = KDistr(signal, inertia).get_sample()
+        samples_obj = Samples(signal, inertia)
+        all, poss, negs = samples_obj.get_all_pos_neg()
         fig, ax = plt.subplots()
-        ax.hist(sample)
+        ax.hist(all, color='gray', alpha=0.3)
+        ax.hist(poss, label="+", color='green', alpha=0.5)
+        ax.hist(negs, label="-", color='red', alpha=0.5)
+        ax.legend()
         log.add_text("Инерция = " + str(inertia) + " :")
         log.add_fig(fig)
